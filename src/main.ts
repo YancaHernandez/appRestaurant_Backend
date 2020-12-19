d0e7b7623a4b7cb856ed34e44ed75802e59bc669
Yan Hernandez <yanhernandez.1997@gmail.com>
2020-12-18 22:43:36 -0500

    Primer_Commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttExceptionFilter } from './shared/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttExceptionFilter());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 3500);
+}
+
+bootstrap();
