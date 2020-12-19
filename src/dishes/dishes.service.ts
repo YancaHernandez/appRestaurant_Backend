31a4165da592be045e38baf739d1198be810779f
Yan Hernandez <yanhernandez.1997@gmail.com>
2020-12-19 04:23:29 -0500

    APIPUBLIC

--- src/dishes/dishes.service.ts
+++ src/dishes/dishes.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Dishes } from '../types/Dishes';
+import { Dishes } from '../types/dishes';
 import { CreateDishesDto as CreateDto,UpdateDishesDto as UpdateDto } from './dishes.dto';
-
 @Injectable()
 export class DishesService {
     constructor(@InjectModel('Dishes') private model: Model<Dishes>) {}
